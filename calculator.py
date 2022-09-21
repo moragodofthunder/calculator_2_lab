08f1e253862bdbfe425c4e021b3c3ccdf5bcb08a
Mora <mora@Marshalls-MBP.lan>
2022-09-21 16:08:16 -0700

    Added addition token statement

---
+++ calculator.py
@@ -0,0 +1,13 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+user_input = input("What do you want to calculate?: ")
+token = user_input.split(" ")
+
+if token[0] == "+":
+    print(add(int(token[1]), int(token[2])))
+
+
+# Replace this with your code
