135bc6d4e2d2fb23df044a8a8fb41bed02381c73
Mora <moragodofthunder@gmail.com>
2022-09-21 16:33:58 -0700

    Added while True loop

--- calculator.py
+++ calculator.py
@@ -3,29 +3,36 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-user_input = input("What do you want to calculate?: ")
-token = user_input.split(" ")
+while True:
+    user_input = input("What do you want to calculate?: ")
+    token = user_input.split(" ")
 
-if token[0] == "+":
-    print(add(int(token[1]), int(token[2])))
+    if token[0] == "+":
+        print(add(int(token[1]), int(token[2])))
 
-elif token[0] == "-":
-    print(subtract(int(token[1]), int(token[2])))
+    elif token[0] == "-":
+        print(subtract(int(token[1]), int(token[2])))
 
-elif token[0] == "*":
-    print(multiply(int(token[1]), int(token[2])))
+    elif token[0] == "*":
+        print(multiply(int(token[1]), int(token[2])))
 
-elif token[0] == "/":
-    print(divide(int(token[1]), int(token[2])))
+    elif token[0] == "/":
+        print(divide(int(token[1]), int(token[2])))
 
-elif token[0] == "cube":
-    print(cube(int(token[1]), int(token[2])))
+    elif token[0] == "cube":
+        print(cube(int(token[1]), int(token[2])))
 
-elif token[0] == "square":
-    print(square(int(token[1]), int(token[2])))
+    elif token[0] == "square":
+        print(square(int(token[1]), int(token[2])))
 
-elif token[0] == "power":
-    print(power(int(token[1]), int(token[2])))
+    elif token[0] == "power":
+        print(power(int(token[1]), int(token[2])))
 
-elif token[0] == "modulo":
-    print(mod(int(token[1]), int(token[2])))
+    elif token[0] == "mod":
+        print(mod(int(token[1]), int(token[2])))
+
+    elif "q" in token:
+        print("Okay bye")
+        break
+    else:
+        print("Sorry, that's invalid")
